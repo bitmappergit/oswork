70478c9148ebd25473d888cc3acdd986dc360be5
Bram Wyllie <bitmapper85@gmail.com>
2018-06-27 11:32:10 -0300

    haha yes

--- fb.h
+++ fb.h
@@ -1,9 +1,9 @@
+#ifndef INCLUDE_FB_H
+#define INCLUDE_FB_H
+
 #include <stddef.h>
 #include <stdint.h>
 #include "io.h"
-
-#ifndef FB_H
-#define FB_H
 
 #define FB_COMMAND_PORT	0x3D4
 #define FB_DATA_PORT		0x3D5
@@ -14,11 +14,11 @@
 #define FB_BLACK 0
 #define FB_WHITE 15
 
-#endif
-
 void fb_init(void);
 void fb_write_cell(uint8_t x, uint8_t y, char c, unsigned char fg, unsigned char bg);
 void fb_move_cursor(int x, int y);
 void fb_write_char(char c);
 void fb_write_str(char* buf);
 void fb_clear();
+
+#endif
